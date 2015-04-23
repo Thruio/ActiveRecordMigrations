0e7c66a52fa0b9f8c73f25420354e1486d367db2
Matthew Baggett <matthew@baggett.me>
2015-04-23 16:48:25 +0100

    partially working export.

--- Thru/ActiveRecordMigrations/Exporter.php
+++ Thru/ActiveRecordMigrations/Exporter.php
@@ -1,2 +1,32 @@
 <?php
 
+namespace Thru\ActiveRecordMigrations;
+
+use Thru\ActiveRecord\ActiveRecord;
+use Thru\ActiveRecord\DumbModel;
+
+class Exporter{
+	static public function Run($class, $output_dir){
+		$object = new $class();
+		if($object instanceof ActiveRecord){
+			$data = $object::search()->exec();
+			$data_array = array();
+
+			foreach($data as $row){
+				/* @var $row ActiveRecord */
+				$schema = array_keys($row->get_class_schema());
+				$data_array[] = $row->__ToArray($schema);
+			}
+			if(!file_exists($output_dir)){
+				mkdir($output_dir, 0777, true);
+			}
+			$output_file = $object->get_class(true) . ".json";
+			$json = json_encode($data_array);
+			file_put_contents($output_dir . "/" . $output_file, $json);
+			echo "Written " . strlen($json) . " bytes to {$output_file}\n";
+
+		}else{
+			die("Not an ActiveRecord object\n\n");
+		}
+	}
+}
--- Thru/ActiveRecordMigrations/Migrator.php
+++ Thru/ActiveRecordMigrations/Migrator.php
@@ -2,9 +2,42 @@
 
 namespace Thru\ActiveRecordMigrations;
 
+use Commando\Command;
+
 class Migrator
 {
 	static public function Main(){
+
+		$migrator_command = new Command();
+		$migrator_command->option('i')
+			->aka('import')
+			->describedAs("Import migrations from .")
+			->boolean();
+		$migrator_command->option('e')
+			->aka('export')
+			->describedAs("Export migrations from .")
+			->boolean();
+		$migrator_command->option('class')
+			->require();
+		$migrator_command->option('migration_path')->aka('output')
+			->require()
+			->default("migrations");
+
+		if($migrator_command['export']){
+			$classes_to_compute = [];
+
+			if($migrator_command['class']){
+				$classes_to_compute = $migrator_command['class'];
+			}else{
+				yaml
+			}
+			foreach($classes_to_compute as $class){
+				Exporter::Run($class, $migrator_command['migration_path']);
+			}
+
+		}else{
+			$migrator_command->printHelp();
+		}
 
 	}
 }
