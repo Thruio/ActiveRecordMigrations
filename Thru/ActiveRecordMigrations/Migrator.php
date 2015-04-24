8ff40beed05c4286c0516a7b3022cbd01587818e
Matthew Baggett <matthew@baggett.me>
2015-04-24 13:11:12 +0100

    Added --purge command

--- Thru/ActiveRecordMigrations/Migrator.php
+++ Thru/ActiveRecordMigrations/Migrator.php
@@ -4,10 +4,12 @@ namespace Thru\ActiveRecordMigrations;
 
 use Commando\Command;
 use Symfony\Component\Yaml\Yaml;
+use Thru\ActiveRecord\ActiveRecord;
 
 class Migrator
 {
 	static public function Main(){
+		$start = microtime(true);
 
 		$migrator_command = new Command();
 		$migrator_command->option('i')
@@ -20,6 +22,11 @@ class Migrator
 			->boolean();
 		$migrator_command->option('class')
 			->describedAs("fill this in");
+
+		$migrator_command->option('p')->aka('purge')
+			->boolean()
+			->describedAs("Purge tables before import & rebuild");
+
 		$migrator_command->option('migration_path')->aka('output')
 			->require()
 			->default("fixtures");
@@ -35,17 +42,37 @@ class Migrator
 			}
 		}
 
+		if($migrator_command['purge']){
+			foreach ($classes_to_compute as $class){
+				Migrator::Purge($class);
+			}
+		}
+
 		if($migrator_command['export']) {
 			foreach ($classes_to_compute as $class) {
 				Exporter::Run($class, $migrator_command['migration_path']);
 			}
+			$end = microtime(true);
+			$time_to_execute = number_format($end - $start, 2);
+			echo "Export finished in {$time_to_execute} seconds\n\n";
 		}elseif($migrator_command['import']) {
 			foreach ($classes_to_compute as $class) {
 				Importer::Run($class, $migrator_command['migration_path']);
 			}
+			$end = microtime(true);
+			$time_to_execute = number_format($end - $start, 2);
+			echo "Import finished in {$time_to_execute} seconds\n\n";
 		}else{
 			$migrator_command->printHelp();
 		}
 
+	}
+
+	static public function Purge($class){
+		/* @var $o ActiveRecord */
+		$o = new $class();
+		echo "Purging {$class} table {$o->get_table_name()}";
+		$o->delete_table();
+		echo " [Done]\n";
 	}
 }
