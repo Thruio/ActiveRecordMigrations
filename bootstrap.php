b5ec5504d96dfcef2fbb819ccaf455e710d90f55
Matthew Baggett <matthew@baggett.me>
2015-04-27 18:05:26 +0100

    Initial testing, Move to PSR4

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$database = new \Thru\ActiveRecord\DatabaseLayer(array(
+  'db_type'     => 'Mysql',
+  'db_hostname' => 'localhost',
+  'db_port'     => '3306',
+  'db_username' => 'travis',
+  'db_password' => 'travis',
+  'db_database' => 'active_record_migrations_test'
+));
+
---
+++ tests/DumbTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Thru\ActiveRecord\Test;
+
+
+class DumbTest extends BaseTest {
+
+  public function testNothing(){
+    $this->assertTrue(true);
+  }
+}
